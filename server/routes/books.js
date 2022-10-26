a3b8b6d4c8df26f0e523db7bed7218f41e19de6b
awarena <76248192+awarena@users.noreply.github.com>
2022-10-26 18:25:19 -0400

    add function for edit book

--- server/routes/books.js
+++ server/routes/books.js
@@ -52,17 +52,43 @@ router.post('/add', (req, res, next) => {
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+  let id = req.params.id;
+
+  book.findById(id, (err, bookToEdit) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    }
+    else {
+      //show the edit view
+      res.render('books/details', { title: 'Edit Book', books: bookToEdit })
+    }
+  });
 });
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
 
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+  let id = req.params.id
+
+  let updatedBook = book({
+    "_id": id,
+    "Title": req.body.title,
+    "Price": req.body.price,
+    "Author": req.body.author,
+    "Genre": req.body.genre
+  });
+
+  book.updateOne({ _id: id }, updatedBook, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    }
+    else {
+      // refresh the book list
+      res.redirect('/books');
+    }
+  });
 
 });
 
